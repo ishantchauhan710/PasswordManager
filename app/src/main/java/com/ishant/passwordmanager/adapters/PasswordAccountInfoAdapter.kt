2ba98d36453cb7b44b71c17cc6d609e56ae49481
Ishant <ishantchauhan710@gmail.com>
2021-09-03 13:51:34 +0530

    Account Details Recyclerview Implemented

--- app/src/main/java/com/ishant/passwordmanager/adapters/PasswordAccountInfoAdapter.kt
+++ app/src/main/java/com/ishant/passwordmanager/adapters/PasswordAccountInfoAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.ishant.passwordmanager.R
 import com.ishant.passwordmanager.databinding.LayoutAccountInfoBinding
 import com.ishant.passwordmanager.db.entities.AccountDetails
 
@@ -32,10 +33,26 @@ class PasswordAccountInfoAdapter: RecyclerView.Adapter<PasswordAccountInfoAdapte
     }
 
     override fun getItemCount(): Int {
-        return 20
+        return differ.currentList.size
     }
 
     override fun onBindViewHolder(holder: PasswordAccountInfoAdapterViewHolder, position: Int) {
+        val detail = differ.currentList[position]
+        holder.binding.tvInfoType.text = detail.detailType
+        holder.binding.tvInfoContent.text = detail.detailContent
+
+        when(holder.binding.tvInfoType.text) {
+            "Username" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_username_info)
+            "Email" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_mail_info)
+            "Phone Number" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_phone_info)
+            "Password" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_password_change)
+            "Website" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_website_info)
+            "Note" -> holder.binding.ivInfoIcon.setImageResource(R.drawable.ic_note_info)
+        }
+
+        holder.binding.btnDeleteInfo.setOnClickListener {
+            differ.currentList.removeAt(holder.adapterPosition)
+        }
 
     }
 }
--- app/src/main/java/com/ishant/passwordmanager/db/entities/AccountDetails.kt
+++ app/src/main/java/com/ishant/passwordmanager/db/entities/AccountDetails.kt
@@ -3,6 +3,6 @@ package com.ishant.passwordmanager.db.entities
 data class AccountDetails (
     val id: Int,
     val passwordId: Int,
-    val detailType: Int,
+    val detailType: String,
     val detailContent: String
 )
