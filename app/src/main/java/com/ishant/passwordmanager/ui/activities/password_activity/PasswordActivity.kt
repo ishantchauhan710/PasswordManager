3ab6b86ee17322feef0bb0b510482db347500415
Ishant <ishantchauhan710@gmail.com>
2021-09-04 15:03:04 +0530

    Double Encryption Tested

--- app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/PasswordActivity.kt
+++ app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/PasswordActivity.kt
@@ -56,24 +56,44 @@ class PasswordActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-        val password = "ishant"
+        // Variable Names
+        // emd: Encrypt Main Data
+        // eed: Encrypt Encrypted Data
+        // This is the main data of user that will be encrypted. It will include emails, passwords, usernames etc
         val dataToEncrypt = "This is data to Encrypt"
+        // This is a password to generate key
+        val emdPassword = "ishant"
+        // A random salt is generated which will be used in creating key
+        val emdSalt = saltString(generateSalt())
+        // A key is generated using the password and salt
+        val emdKey = generateKeyFromPassword(emdPassword,emdSalt)
+        // Our main data will get encrypted using the key we generated
+        val emdEncryptedString = encrypt(dataToEncrypt, emdKey).toString()
+        // Now we will again encrypt this encrypted string
+        // This is a new password to create a new key to encrypt the encrypted string
+        val eedPassword = "chauhan"
+        // Another salt generated to generate new key to encrypt the encrypted string
+        val eedSalt = saltString(generateSalt())
+        // New key is generated using new password and new salt
+        val eedKey = generateKeyFromPassword(eedPassword,eedSalt)
+        // Now here we will encrypt the encrypted string
+        val eedEncryptedString = encrypt(emdEncryptedString, eedKey).toString()
+        // Now we need to decrypt the data
+        // This will return the first level encrypted string of our main data
+        val eedDecryptedString = decryptString(CipherTextIvMac(eedEncryptedString),eedKey)
+        val emdDecryptedString = decryptString(CipherTextIvMac(emdEncryptedString),emdKey)
+        val finalDecryptedString = emdDecryptedString
+        Log.e("IshantEncryption","Password 1 (EMD): $emdPassword\n")
+        Log.e("IshantEncryption","Password 2 (EED): $eedPassword\n")
+        Log.e("IshantEncryption","Data to Encrypt: $dataToEncrypt\n")
+        Log.e("IshantEncryption","Key1 (EMD): $emdKey\n")
+        Log.e("IshantEncryption","Key2 (EED): $eedKey\n")
+        Log.e("IshantEncryption","Encrypted Data String1 (EMD): $emdEncryptedString\n")
+        Log.e("IshantEncryption","Encrypted Data String2 (EED): $eedEncryptedString\n")
+        Log.e("IshantEncryption","Main Data: $dataToEncrypt\n")
+        Log.e("IshantEncryption","Final Decrypted String: $finalDecryptedString\n")
 
-        val salt = saltString(generateSalt())
-        val key = generateKeyFromPassword(password, salt)
-        val encryptedDataString = encrypt(dataToEncrypt, key).toString()
 
-        val encryptedData = CipherTextIvMac(encryptedDataString)
-        val decryptedData = decryptString(encryptedData, key)
-
-        Log.e("IshantEncryption", "Password: $password\n")
-        Log.e("IshantEncryption", "Data To Encrypt: $dataToEncrypt\n")
-        Log.e("IshantEncryption", "Salt: $salt\n")
-        Log.e("IshantEncryption", "Key: $key\n")
-        Log.e("IshantEncryption", "Encrypted Data String: $encryptedDataString\n")
-        Log.e("IshantEncryption", "Encrypted Data: $encryptedData\n")
-        Log.e("IshantEncryption", "Decrypted Data: $decryptedData\n")
 
 
 
