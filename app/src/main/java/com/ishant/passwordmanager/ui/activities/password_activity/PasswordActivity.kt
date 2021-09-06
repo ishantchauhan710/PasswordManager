cd0a74bd4726d6986b0d6fcca62b62043d6663e2
Ishant <ishantchauhan710@gmail.com>
2021-09-07 01:18:14 +0530

    Drawer Layout Design Changed (Badge Icon Added)

--- app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/PasswordActivity.kt
+++ app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/PasswordActivity.kt
@@ -3,9 +3,11 @@ package com.ishant.passwordmanager.ui.activities.password_activity
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -14,24 +16,29 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ishant.passwordmanager.R
-import com.ishant.passwordmanager.adapters.PasswordAdapter
 import com.ishant.passwordmanager.databinding.ActivityPasswordBinding
 import com.ishant.passwordmanager.db.PasswordManagerDatabase
 import com.ishant.passwordmanager.db.entities.Entry
 import com.ishant.passwordmanager.repository.PasswordManagerRepository
 import com.ishant.passwordmanager.ui.activities.create_edit_view_password_activity.CreateEditViewPasswordActivity
-import com.ishant.passwordmanager.ui.activities.password_activity.fragments.SearchPasswordFragment
 import com.ishant.passwordmanager.ui.factories.CreateEditViewPasswordViewModelProviderFactory
 import com.ishant.passwordmanager.ui.viewmodels.CreateEditViewPasswordViewModel
+import com.mikepenz.materialdrawer.holder.*
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.iconDrawable
+import com.mikepenz.materialdrawer.model.interfaces.nameRes
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
 import com.tozny.crypto.android.AesCbcWithIntegrity.*
-import java.security.KeyStore
 
 
 class PasswordActivity : AppCompatActivity() {
@@ -90,6 +97,100 @@ class PasswordActivity : AppCompatActivity() {
         }
 
 
+        // Header Layout
+
+        val headerView = AccountHeaderView(this).apply {
+            attachToSliderView(binding.navView) // attach to the slider
+            headerBackground =  ImageHolder(R.drawable.tech_bg)
+            onAccountHeaderListener = { view, profile, current ->
+                // react to profile changes
+                false
+            }
+            withSavedInstance(savedInstanceState)
+        }
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        val item1 = PrimaryDrawerItem().apply {
+            nameRes = R.string.all
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_all)
+            badge = StringHolder("07")
+            badgeStyle = BadgeStyle().apply {
+                textColor = ColorHolder.fromColor(Color.WHITE)
+                color = ColorHolder.fromColor(Color.parseColor("#2F3061"))
+                corners = DimenHolder.fromDp(50)
+            }
+        }
+
+        val item2 = PrimaryDrawerItem().apply {
+            nameRes = R.string.social
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_social)
+        }
+
+        val item3 = PrimaryDrawerItem().apply {
+            nameRes = R.string.mails
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_mail)
+        }
+
+        val item4 = PrimaryDrawerItem().apply {
+            nameRes = R.string.card
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_card)
+        }
+
+        val item5 = PrimaryDrawerItem().apply {
+            nameRes = R.string.work
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_work)
+        }
+
+        val item6 = PrimaryDrawerItem().apply {
+            nameRes = R.string.others
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_others)
+        }
+
+        val item7 = PrimaryDrawerItem().apply {
+            nameRes = R.string.changepassword
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_password_change)
+        }
+
+        val item8 = PrimaryDrawerItem().apply {
+            nameRes = R.string.exit
+            identifier = 1
+            iconDrawable = resources.getDrawable(R.drawable.ic_exit)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        // get the reference to the slider and add the items
+        binding.navView.itemAdapter.add(item1,item2,item3,item4,item5,item6,DividerDrawerItem(),item7,item8)
+
+        // specify a click listener
+        binding.navView.onDrawerItemClickListener = { v, drawerItem, position ->
+            // do something with the clicked item
+            if(position==0) {
+                Toast.makeText(this,"All",Toast.LENGTH_SHORT).show()
+            } else if(position==1) {
+                Toast.makeText(this,"Social",Toast.LENGTH_SHORT).show()
+            }
+
+            false
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -103,7 +204,6 @@ class PasswordActivity : AppCompatActivity() {
             R.id.miSearchButton -> {
 
                 findNavController(R.id.fragment).navigate(R.id.searchPasswordFragment)
-
 
 
                 val myActionMenuItem: MenuItem = item
@@ -123,38 +223,38 @@ class PasswordActivity : AppCompatActivity() {
                     return true
                 }
 
-                    val searchCloseIcon: ImageView =
-                        searchView.findViewById(R.id.search_close_btn) as ImageView
-                    searchCloseIcon.setImageResource(R.drawable.ic_clear)
+                val searchCloseIcon: ImageView =
+                    searchView.findViewById(R.id.search_close_btn) as ImageView
+                searchCloseIcon.setImageResource(R.drawable.ic_clear)
 
 
 
-                    searchView.setOnQueryTextListener(object :
-                        androidx.appcompat.widget.SearchView.OnQueryTextListener {
+                searchView.setOnQueryTextListener(object :
+                    androidx.appcompat.widget.SearchView.OnQueryTextListener {
 
-                        override fun onQueryTextSubmit(query: String?): Boolean {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
 
-                            return false
+                        return false
+                    }
+
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+
+                        val emptyList = emptyList<Entry>()
+
+                        if (newText != null && newText != "") {
+                            viewModel.searchEntries(newText)
+                                .observe(this@PasswordActivity, Observer {
+                                    viewModel.filteredSearchList.postValue(it)
+                                })
+                        } else {
+                            viewModel.filteredSearchList.postValue(emptyList)
                         }
 
-
-                        override fun onQueryTextChange(newText: String?): Boolean {
-
-                                val emptyList = emptyList<Entry>()
-
-                                if(newText!=null && newText!="") {
-                                    viewModel.searchEntries(newText)
-                                        .observe(this@PasswordActivity, Observer {
-                                            viewModel.filteredSearchList.postValue(it)
-                                        })
-                                } else {
-                                    viewModel.filteredSearchList.postValue(emptyList)
-                                }
-
-                            return false
-                        }
-                    })
-                }
+                        return false
+                    }
+                })
+            }
             }
             return true
 
@@ -181,12 +281,16 @@ class PasswordActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(upArrow)
     }
 
+
+
+
     private fun setUpNavigationBar() {
         // Navigation Bar Color
         window.navigationBarColor = Color.BLACK
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
+        /*
         binding.navView.setNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.miSocial -> Toast.makeText(applicationContext, "Social", Toast.LENGTH_SHORT)
@@ -199,6 +303,8 @@ class PasswordActivity : AppCompatActivity() {
             }
             true
         }
+
+*/
     }
 
 }
