dabc120a42a68da1433a9c48a58867336f05bec8
Ishant <ishantchauhan710@gmail.com>
2021-09-06 23:53:14 +0530

    Generate Password Fragment Made

--- app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/fragments/GeneratePasswordFragment.kt
+++ app/src/main/java/com/ishant/passwordmanager/ui/activities/password_activity/fragments/GeneratePasswordFragment.kt
@@ -1,18 +1,28 @@
 package com.ishant.passwordmanager.ui.activities.password_activity.fragments
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.slider.LabelFormatter
+import com.google.android.material.slider.Slider
+import com.google.android.material.snackbar.Snackbar
 import com.ishant.passwordmanager.R
 import com.ishant.passwordmanager.databinding.FragmentGeneratePasswordBinding
 import nu.aaro.gustav.passwordstrengthmeter.PasswordStrengthCalculator
-import java.util.regex.Pattern
+import java.util.*
 
 class GeneratePasswordFragment : Fragment(R.layout.fragment_generate_password) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentGeneratePasswordBinding.bind(view)
+
+        var passwordLength = 13
+
 
         binding.passwordInputMeter.setPasswordStrengthCalculator(object :
             PasswordStrengthCalculator {
@@ -37,7 +47,74 @@ class GeneratePasswordFragment : Fragment(R.layout.fragment_generate_password) {
             }
         })
 
+        val generatedPassword = generatePassword(passwordLength,
+            includeUpperCaseLetters = binding.cbUppercase.isChecked,
+            includeLowerCaseLetters = binding.cbLowercase.isChecked,
+            includeSymbols = binding.cbSymbols.isChecked,
+            includeNumbers = binding.cbNumbers.isChecked)
+
         binding.passwordInputMeter.setEditText(binding.etGeneratedPassword)
+        binding.etGeneratedPassword.setText(generatedPassword)
+
+        binding.sliderPasswordStrength.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {
+
+            }
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                passwordLength = slider.value.toInt()
+            }
+
+
+
+        })
+
+        binding.sliderPasswordStrength.addOnChangeListener(object: Slider.OnChangeListener{
+            override fun onValueChange(slider: Slider, value: Float, fromUser: Boolean) {
+                slider.setLabelFormatter(LabelFormatter { it ->
+                    return@LabelFormatter  "${value.toInt()} Letters"
+                })
+            }
+        })
+
+
+
+        binding.btnGeneratePassword.setOnClickListener {
+
+            val generatedPassword = generatePassword(passwordLength,
+                includeUpperCaseLetters = binding.cbUppercase.isChecked,
+                includeLowerCaseLetters = binding.cbLowercase.isChecked,
+                includeSymbols = binding.cbSymbols.isChecked,
+                includeNumbers = binding.cbNumbers.isChecked)
+
+            if(generatedPassword.isBlank()) {
+                if(passwordLength==0) {
+                    Snackbar.make(view,"Password length cannot be zero",Snackbar.LENGTH_SHORT).show()
+                } else {
+                    Snackbar.make(view, "Please check at least one item", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                binding.etGeneratedPassword.setText(generatedPassword)
+            }
+        }
+
+        binding.btnCopyPassword.setOnClickListener {
+            val clipboard: ClipboardManager? = ContextCompat.getSystemService(
+                requireContext(),
+                ClipboardManager::class.java
+            )
+            val clip = ClipData.newPlainText("Generated Password", binding.etGeneratedPassword.text.toString())
+            clipboard?.setPrimaryClip(clip)
+            Snackbar.make(view, "Password Copied to Clipboard", Snackbar.LENGTH_SHORT).show()
+        }
+
+
+
+
+
+
+
 
     }
 
@@ -46,15 +123,42 @@ class GeneratePasswordFragment : Fragment(R.layout.fragment_generate_password) {
             return 0
         } else if (password.length in 1..3) {
             return 1
-        } else if (password.length in 4..8) {
+        } else if (password.length in 4..6) {
             return 2
-        } else if (password.length in 9..12) {
+        } else if (password.length in 7..9) {
             return 3
-        } else if (password.length in 13..18) {
+        } else if (password.length in 10..12) {
             return 4
         } else {
             return 5
         }
     }
+
+
+    fun <E> MutableList<E>.mRandom(): E? = if (size > 0) get(Random().nextInt(size)) else null
+
+       private fun generatePassword(length: Int,includeUpperCaseLetters : Boolean,includeLowerCaseLetters : Boolean,
+                                    includeSymbols : Boolean,  includeNumbers: Boolean) : String {
+            var password = ""
+            val list  = mutableListOf<Int>()
+            if(includeUpperCaseLetters)
+                list.add(0)
+            if(includeLowerCaseLetters)
+                list.add(1)
+            if(includeNumbers)
+                list.add(2)
+            if(includeSymbols)
+                list.add(3)
+
+            for(i in 1..length){
+                when(list.toMutableList().mRandom()){
+                    0-> password += ('A'..'Z').toMutableList().mRandom().toString()
+                    1-> password += ('a'..'z').toMutableList().mRandom().toString()
+                    2-> password += ('0'..'9').toMutableList().mRandom().toString()
+                    3-> password += listOf('!','@','#','$','%','&','*','+','=','-','~','?','/','_').toMutableList().mRandom().toString()
+                }
+            }
+            return password
+        }
 
 }
